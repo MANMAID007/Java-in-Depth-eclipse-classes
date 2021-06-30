77255bba7a5c1bb76d00a5638ade679f992d5bdd
MANMAID007 <youkti.smith@gmail.com>
2021-06-30 14:21:09 +0530

    classes added

---
+++ javaInDepthAdvanced/src/javaInDepthAdvanced/User.java
@@ -0,0 +1,131 @@
+package javaInDepthAdvanced;
+
+public class User {
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private String email;
+	private long patientId;
+	private boolean insured;
+	private long stuffId;
+	private int yearsOfExperience;
+	private String description;
+	private double salary;
+	private long doctorId;
+	private String specialization;
+	private long nurseId;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(long patientId) {
+		this.patientId = patientId;
+	}
+
+	public boolean isInsured() {
+		return insured;
+	}
+
+	public void setInsured(boolean insured) {
+		this.insured = insured;
+	}
+
+	public long getStuffId() {
+		return stuffId;
+	}
+
+	public void setStuffId(long stuffId) {
+		this.stuffId = stuffId;
+	}
+
+	public int getYearsOfExperience() {
+		return yearsOfExperience;
+	}
+
+	public void setYearsOfExperience(int yearsOfExperience) {
+		this.yearsOfExperience = yearsOfExperience;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public long getDoctorId() {
+		return doctorId;
+	}
+
+	public void setDoctorId(long doctorId) {
+		this.doctorId = doctorId;
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+
+	public long getNurseId() {
+		return nurseId;
+	}
+
+	public void setNurseId(long nurseId) {
+		this.nurseId = nurseId;
+	}
+
+}
